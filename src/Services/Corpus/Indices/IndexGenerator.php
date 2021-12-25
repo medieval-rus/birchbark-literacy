b6616c0d6b21667942ec2dd0cdfca029c9e720c4
Anton Dyshkant <vyshkant@gmail.com>
2021-12-26 00:58:54 +0300

    Fixes

--- src/Services/Corpus/Indices/IndexGenerator.php
+++ src/Services/Corpus/Indices/IndexGenerator.php
@@ -82,7 +82,7 @@ final class IndexGenerator implements IndexGeneratorInterface
                                             ),
                                         $analysis
                                     )
-                            );
+                                );
                         }
                     }
                 }
